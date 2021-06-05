092b37c50f41c5174f88558f0642be8359966b29
Peter Laggner <peter.laggner@student.tugraz.at>
2021-06-05 22:26:25 +0200

    add should bool tests and implemenation

---
+++ test/should_bool_test.dart
@@ -0,0 +1,31 @@
+import 'package:dartest/dartest.dart';
+import 'package:test/test.dart';
+
+bool functionReturningTrueValue() => true;
+
+bool falseValue = false;
+
+void main() {
+  group('Happy Path', () {
+    test('shouldBeTrue', () {
+      (() => functionReturningTrueValue().shouldBeTrue())
+          .shouldReturnNormally();
+    });
+
+    test('shouldBeFalse', () {
+      (() => falseValue.shouldBeFalse()).shouldReturnNormally();
+    });
+  });
+
+  group('Unhappy Path', () {
+    test('shouldBeTrue', () {
+      shouldThrow<TestFailure>(
+          () => functionReturningTrueValue().shouldBeFalse());
+    });
+
+    test('shouldBeFalse', () {
+      shouldThrow<TestFailure>(
+          () => falseValue.shouldBeTrue());
+    });
+  });
+}
