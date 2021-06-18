150fa8f514e49e002eb85c01bbf1bfcd63f22b8e
Peter Laggner <peter.laggner@student.tugraz.at>
2021-06-18 23:30:47 +0200

    add doc comments for should bool

--- lib/src/should_bool.dart
+++ lib/src/should_bool.dart
@@ -1,7 +1,11 @@
 import 'package:dartest/src/should.dart';
 
 extension ShouldBool on bool {
+  /// Convenience assertion that the expression is true.
+  /// Equivalent to `expr.shouldBe(true)`
   void shouldBeTrue() => shouldBe(true);
 
+  /// Convenience assertion that the expression is false.
+  /// Equivalent to `expr.shouldBe(false)`
   void shouldBeFalse() => shouldBe(false);
 }
