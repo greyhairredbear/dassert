5280ed0abf03361bc859f503af96edf69caef4bc
Peter Laggner <peter.laggner@gmx.at>
2022-11-16 23:37:46 +0100

    rename to dassert

--- lib/dartest.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-/// TODO
-/// More dartdocs go here.
-library dassert;
-
-export 'src/should.dart';
-export 'src/bool.dart';
-export 'src/throw.dart';
---
+++ lib/dassert.dart
@@ -0,0 +1,7 @@
+/// Dart assertion library inspired by
+/// [Kotest](https://kotest.io/docs/assertions/core-matchers.html).
+library dassert;
+
+export 'src/should.dart';
+export 'src/bool.dart';
+export 'src/throw.dart';
