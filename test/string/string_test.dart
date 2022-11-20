0e4886e5c736a29d142fdd14001ce847d7a2af38
Peter Laggner <peter.laggner@gmx.at>
2022-11-20 22:49:34 +0100

    #13 add test groups for kotest string matchers

---
+++ test/string/string_test.dart
@@ -0,0 +1,94 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('should be blank', () {
+    test('', () {});
+  });
+
+  group('should be empty', () {
+    test('', () {});
+  });
+
+  group('should be lower case', () {
+    test('', () {});
+  });
+
+  group('should be upper case', () {
+    test('', () {});
+  });
+
+  group('should contain substring', () {
+    test('', () {});
+  });
+
+  group('should contain regex', () {
+    test('', () {});
+  });
+
+  group('should contain a digit', () {
+    test('', () {});
+  });
+
+  group('should contain substring ignoring case', () {
+    test('', () {});
+  });
+
+  group('should contain only digits', () {
+    test('', () {});
+  });
+
+  group('should be integer (and return) - todo: radix param?', () {
+    test('', () {});
+  });
+
+  group('should contain substring exactly once', () {
+    test('', () {});
+  });
+
+  group('should end with suffix', () {
+    test('', () {});
+  });
+
+  group('should have length', () {
+    test('', () {});
+  });
+
+  group('should have line count', () {
+    test('', () {});
+  });
+
+  // TODO group length related stuff into separate file
+  group('should have max length', () {
+    test('', () {});
+  });
+
+  group('should have min length', () {
+    test('', () {});
+  });
+
+  group('should have same length as other string', () {
+    test('', () {});
+  });
+
+  // TODO group regex stuff into separate file
+  group('should match regex', () {
+    test('', () {});
+  });
+
+  // TODO group contains/starts/ends etc?
+  group('should start with prefix', () {
+    test('', () {});
+  });
+
+  group('should be equal ignoring case', () {
+    test('', () {});
+  });
+
+  group('should be truthy', () {
+    test('', () {});
+  });
+
+  group('should be falsy', () {
+    test('', () {});
+  });
+}
