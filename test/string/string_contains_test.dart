9cc4137dc82599cc9b2170fb1238a5df2e53894c
Peter Laggner <peter.laggner@gmx.at>
2022-12-27 19:13:17 +0100

    #13 refactor contains test to use test spec

--- test/string/string_contains_test.dart
+++ test/string/string_contains_test.dart
@@ -2,101 +2,84 @@ import 'package:dassert/dassert.dart';
 import 'package:dassert/src/internal/should_fail.dart';
 import 'package:test/test.dart';
 
-// TODO: #13 refactor to use testspec?
-// class _TestSpec {
-//   final String name;
-//   final String input;
-//   final String containedString;
-//
-//   _TestSpec({required this.name, required this.input, required this.containedString});
-// }
+class _TestSpec {
+  final String name;
+  final String input;
+  final String containedString;
+
+  _TestSpec({required this.name, required this.input, required this.containedString});
+}
 
 void main() {
+  final nonMatchingCaseSpec = _TestSpec(
+    name: 'fail if string not matching case is contained',
+    input: 'cAmEl',
+    containedString: 'camel',
+  );
+
+  final successSpecs = [
+    _TestSpec(
+      name: 'empty string should be contained in empty string',
+      input: '',
+      containedString: '',
+    ),
+    _TestSpec(
+      name: 'empty string should be contained in arbitrary string',
+      input: 'abcde123',
+      containedString: '',
+    ),
+    _TestSpec(
+      name: 'string should contain itself',
+      input: 'deadBEEF',
+      containedString: 'deadBEEF',
+    ),
+    _TestSpec(name: 'should contain prefix', input: 'testString', containedString: 'test'),
+    _TestSpec(name: 'should contain postfix', input: 'testString', containedString: 'String'),
+    _TestSpec(name: 'should contain middle', input: 'test', containedString: 'es'),
+    _TestSpec(name: 'should contain case sensitive', input: 'TeSt', containedString: 'eS'),
+  ];
+  final failSpecs = [
+    _TestSpec(
+      name: 'fail if string is not contained',
+      input: '1234',
+      containedString: 'substring',
+    ),
+    _TestSpec(
+      name: 'should fail if string is contained in reverse order',
+      input: 'dc',
+      containedString: 'cd',
+    ),
+  ];
+
   group('should contain substring', () {
-    test('empty string should be contained in empty string', () {
-      ''.shouldContainSubstring('');
-    });
-
-    test('empty string should be contained in arbitrary string', () {
-      'abcde54321'.shouldContainSubstring('');
-    });
-
-    test('string should contain itself', () {
-      'deadBEEF'.shouldContainSubstring('deadBEEF');
-    });
-
-    test('should contain prefix', () {
-      'testString'.shouldContainSubstring('test');
-    });
-
-    test('should contain postfix', () {
-      'testString'.shouldContainSubstring('String');
-    });
-
-    test('should contain middle', () {
-      'test'.shouldContainSubstring('es');
-    });
-
-    test('should contain case sensitive', () {
-      'TeSt'.shouldContainSubstring('eS');
-    });
-
-    test('should fail if string is not contained', () {
-      shouldFail(() => '1234'.shouldContainSubstring('substring'));
-    });
-
-    test('should fail if string not matching case is contained', () {
-      shouldFail(() => 'cAmEl'.shouldContainSubstring('camel'));
-    });
-
-    test('should fail if string is contained in reverse order', () {
-      shouldFail(() => 'dc'.shouldContainSubstring('cd'));
-    });
+    for (final spec in successSpecs) {
+      test(spec.name, () => spec.input.shouldContainSubstring(spec.containedString));
+    }
+    for (final spec in failSpecs + [nonMatchingCaseSpec]) {
+      test(
+        spec.name,
+        () => shouldFail(() => spec.input.shouldContainSubstring(spec.containedString)),
+      );
+    }
   });
 
   group('should contain substring ignoring case', () {
-    test('empty string should be contained in empty string', () {
-      ''.shouldContainIgnoringCase('');
-    });
-
-    test('empty string should be contained in arbitrary string', () {
-      'abcde54321'.shouldContainIgnoringCase('');
-    });
-
-    test('string should contain itself', () {
-      'deadBEEF'.shouldContainIgnoringCase('deadBEEF');
-    });
-
-    test('should contain prefix', () {
-      'testString'.shouldContainIgnoringCase('test');
-    });
-
-    test('should contain postfix', () {
-      'testString'.shouldContainIgnoringCase('String');
-    });
-
-    test('should contain middle', () {
-      'test'.shouldContainIgnoringCase('es');
-    });
-
-    test('should contain matching case', () {
-      'TeSt'.shouldContainIgnoringCase('eS');
-    });
-
-    test('should contain ignoring case', () {
-      'cAmEl'.shouldContainIgnoringCase('camel');
-    });
-
-    test('should fail if string is not contained', () {
-      shouldFail(() => '1234'.shouldContainIgnoringCase('substring'));
-    });
-
-    test('should fail if string is contained in reverse order', () {
-      shouldFail(() => 'dc'.shouldContainIgnoringCase('cd'));
-    });
+    for (final spec in successSpecs + [nonMatchingCaseSpec]) {
+      test(spec.name, () => spec.input.shouldContainIgnoringCase(spec.containedString));
+    }
+    for (final spec in failSpecs) {
+      test(
+        spec.name,
+        () => shouldFail(() => spec.input.shouldContainIgnoringCase(spec.containedString)),
+      );
+    }
   });
 
-  /*
+  group('should contain substring exactly once', () {
+    test('', () {});
+  });
+
+/*
 
   group('should contain a digit', () {
     test('', () {});
@@ -106,8 +89,5 @@ void main() {
     test('', () {});
   });
 
-  group('should contain substring exactly once', () {
-    test('', () {});
-  });
    */
 }
