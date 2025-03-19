d379cda2b6b3e5ef5a2ac7da681df2c175b51e10
Peter Laggner <peter.laggner@cargonexx.de>
2025-03-19 15:19:16 +0100

    feat: collection shouldContainNoNulls

--- lib/src/public/collections/collections.dart
+++ lib/src/public/collections/collections.dart
@@ -1,10 +1,11 @@
 import 'package:dassert/dassert.dart';
 import 'package:test/expect.dart';
 
-/// Utils for asserting [num] values.
+/// Utils for asserting [Iterable] values.
 extension IterableMatchers<C extends Iterable<E?>, E> on C {
   /// Asserts that the collection contains no null elements, or is empty.
-  C shouldContainNoNulls() => should(predicate((C p0) => !p0.contains(null)));
+  C shouldContainNoNulls() =>
+      should(predicate((C p0) => !p0.contains(null), 'contains no elements that are null'));
 
   /// Asserts that the collection contains at least one null element.
 // collection.shouldContainNull()
--- test/collections/collections_test.dart
+++ test/collections/collections_test.dart
@@ -1,1 +1,30 @@
-// TODO: fail success specs for null tests
+import 'dart:math';
+
+import 'package:dassert/dassert.dart';
+
+import '../util/run_spec.dart';
+
+class _TestSpec<E> extends BaseTestSpec {
+  final Iterable<E> input;
+
+  _TestSpec({required String name, required this.input}) : super(name);
+}
+
+void main() {
+  runSpecs(
+    'should contain no nulls',
+    successSpecs: [
+      _TestSpec(name: 'empty', input: []),
+      _TestSpec(name: 'empty', input: [1, 2, 3]),
+    ],
+    failSpecs: [
+      _TestSpec(name: 'one null value', input: [null]),
+      _TestSpec(name: 'null value in the middle', input: [e, pi, null, ln2, sqrt2]),
+    ],
+    testFunction: (_TestSpec<dynamic> spec) => spec.input.shouldContainNoNulls(),
+  );
+
+  // TODO: fix usages of predicate function (provide string explanations for predicate)
+
+  // TODO: other tests
+}
